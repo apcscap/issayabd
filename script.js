81c543c0efa59878ab7c291c80f222deb8d16e7a
calee14 <caplee1000@gmail.com>
2020-12-04 14:01:35 -0800

    changing some cards

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function flip12() {
 }
 
 function flip23() {
-    $('#next12').prop('disabled', true)
+    $('#next23').prop('disabled', true)
     var audio = new Audio('https://www.soundjay.com/misc/sounds/page-flip-01a.mp3');
     audio.volume = 0.3
     audio.play();
@@ -26,6 +26,22 @@ function flip23() {
         //do something special
         $('#card2').hide();
         $('#card3').show();
+    }, 1000);
+    
+}
+
+function flip34() {
+    $('#next34').prop('disabled', true)
+    var audio = new Audio('https://www.soundjay.com/misc/sounds/page-flip-01a.mp3');
+    audio.volume = 0.3
+    audio.play();
+    
+    setTimeout(
+    function() 
+    {
+        //do something special
+        $('#card3').hide();
+        $('#card4').show();
     }, 1000);
     
 }
