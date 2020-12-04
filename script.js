4f64b063671554f860745894fdff80c3842177f1
calee14 <caplee1000@gmail.com>
2020-12-04 13:50:01 -0800

    fix cards

---
+++ script.js
@@ -0,0 +1,31 @@
+function flip12() {
+    $('#next12').prop('disabled', true)
+    var audio = new Audio('https://www.soundjay.com/misc/sounds/page-flip-01a.mp3');
+    audio.volume = 0.3
+    audio.play();
+    
+    setTimeout(
+    function() 
+    {
+        //do something special
+        $('#card1').hide();
+        $('#card2').show();
+    }, 1000);
+    
+}
+
+function flip23() {
+    $('#next12').prop('disabled', true)
+    var audio = new Audio('https://www.soundjay.com/misc/sounds/page-flip-01a.mp3');
+    audio.volume = 0.3
+    audio.play();
+    
+    setTimeout(
+    function() 
+    {
+        //do something special
+        $('#card2').hide();
+        $('#card3').show();
+    }, 1000);
+    
+}
